b53c93c492ef69d5ccc546c3e5718ff8f1c3525f
Marco Ruiz <mrmcr82@hotmail.com>
2014-11-24 22:09:03 -0600

    Update enoteProcessEmail.pl

--- enoteProcessEmail.pl
+++ enoteProcessEmail.pl
@@ -1,4 +1,12 @@
 #!/usr/bin/perl
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# File: 		enoteProcessingEmail.pl
+# Description: 	Script that processes the incoming notification emails from Enote
+# 				and upload the detected events in a cloud DB for later processing.
+# Language:		Perl
+# Author:		Marco Ruiz Mora
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
 use strict;
 use warnings;
 use lib "/home/rumarco/usr/lib64/perl5";
@@ -6,17 +14,29 @@ use lib "/home/rumarco/usr/lib64/perl5";
 use DBI;
 use POSIX qw(strftime);
 
+#Database connection statement
 my $dbh = DBI->connect('dbi:mysql:dbname=gpsinternaldb;port=1531;host=g1t4741.austin.hp.com','gpsinternaldb','Welcome-1234',{AutoCommit=>0,RaiseError=>0,PrintError=>1});
 
+#Init of variables
 my $ENOTE_MAIL_BYTES_FILE = '/home/rumarco/enote_mail_bytes.tmp';
 my $last_byte;
 my $line;
 my $alert_page;
-my $ll;
-my $lx;
 my $serverDate = "";
 my $sth;
+my $dispatched_ticket_id_body;
+my $dispatched_ticket_workgroup;
+my $dispatched_ticket_id;
+my $ticket_dispatched_prio;
+my $update_statement;
+my $rv;
+my $ttr_ticket_id;
+my $ttr_ticket_prio;
+my $ttr_percent;
+my $sms_ttr_subject;
+my $full_ticket_subject;
 
+#Checking if pointer for mail spool file exists and performing actions according to condition
 if (-e $ENOTE_MAIL_BYTES_FILE)
 {
         open(MY_ENOTE_FILE, $ENOTE_MAIL_BYTES_FILE)
@@ -36,6 +56,7 @@ else
         $last_byte = 0;
 }
 
+#Declaring mail spool file to check for enote alerts and checking it since last line read
 my $FILE = '/var/spool/mail/rumarco';
 open (INFILE, $FILE) || die "Not able to open the file: $FILE \n";
 for (;;)
@@ -54,35 +75,49 @@ for (;;)
     	my $line = $_;
         chomp($line);
         
+        #Condition that searches for SLO emails and make insert of ticket into into db speficied for it
         if ($line =~ m/^Subject:\s([\w|\-|\s|\d]+\s:\s(N-IM[\d|-]+)\s\(.+\)\s-\s(TTR\s[\w|\d|-|\s]+))/)
        	{
         	$serverDate = strftime("%m/%d/%Y %I:%M %p", localtime());
+        	$ttr_ticket_id = $2;
+        	$ttr_ticket_prio = $3;
+        	$ttr_percent = $4;
+        	$sms_ttr_subject = "ALERT: Incident $ttr_ticket_id $ttr_ticket_prio with $ttr_percent";
         	$sth = $dbh->prepare("INSERT INTO ticket_with_slo
                        (ticket_id, ticket_subject, ticket_workgroup, ticket_sent_pager, ticket_date_added_db, sms_message_to_mobile, ticket_date_sent_page)
                         values
                        (?, ?, ?, ?, ?, ?,?)");
-			$sth->execute($2, $3, 'null', 'N', $serverDate, $1,'null') or die $DBI::errstr;
+			$sth->execute($ttr_ticket_id, $1, 'null', 'N', $serverDate, $sms_ttr_subject,'null') or die $DBI::errstr;
 			$sth->finish();
 			$dbh->commit or die $DBI::errstr;
-        	#print "ALERT: $1 $2 $3\n";
-        	#print "ALERT: $1\n";
-        	
+        	#print "ALERT: $1\n";        	
         }
-        if ($line =~ m/^Subject:\s([\w|\-|\s|\d]+\s:\s(N-IM[\d|-]+)\s\(.+\)\s-\sDispatched)/)
+        #Condition that searches for dispatched emails and gets priority and ticket it
+        if ($line =~ m/^Subject:\s([\w|\-|\s|\d]+\s:\s(N-IM[\d|-]+)\s(\(.+\))\s-\sDispatched)/)
         {
 			$serverDate = strftime("%m/%d/%Y %I:%M %p", localtime());
-			$sth = $dbh->prepare("INSERT INTO ticket_in_dispatched
-                       (ticket_id, ticket_subject, ticket_workgroup, ticket_sent_pager, ticket_date_added_db, sms_message_to_mobile,ticket_date_sent_page )
-                        values
-                       (?, ?, ?, ?, ?, ?,?)");
-			$sth->execute($2, 'null', 'null', 'N', $serverDate, $1,'null') or die $DBI::errstr;
-			$sth->finish();
-			$dbh->commit or die $DBI::errstr;
-			#print "ALERT: $2\n";
-			#print "ALERT: $1 $2\n";
+			$dispatched_ticket_id = $2;
+			$ticket_dispatched_prio = $3;
+			$full_ticket_subject = $1;
         }
+        #Condition that searches for workgroup in dispatched email body and compares it with $dispatched_ticket_id
+        if ($line =~ m/(^N-IM[\d|-]+)\shas been\sdispatched\sto\sassignment\sgroup\s?([\w\d|-]+)/)
+        {
+			$dispatched_ticket_id_body = $1;
+			$dispatched_ticket_workgroup = $2;
+			if ($dispatched_ticket_id eq $dispatched_ticket_id_body)
+			{
+				my $sms_subject = "ALERT: Incident $dispatched_ticket_id $ticket_dispatched_prio assigned to $2\n";
+				$sth = $dbh->prepare("INSERT INTO ticket_in_dispatched
+            	          (ticket_id, ticket_subject, ticket_workgroup, ticket_sent_pager, ticket_date_added_db, sms_message_to_mobile,ticket_date_sent_page )
+            	            values
+                	       (?, ?, ?, ?, ?, ?,?)");
+				$sth->execute($dispatched_ticket_id, $full_ticket_subject, $dispatched_ticket_workgroup, 'N', $serverDate, $sms_subject,'null') or die $DBI::errstr;
+				$sth->finish();
+				$dbh->commit or die $DBI::errstr;
+			}			
+        }        
         ##### FOR DTV PAGING #####
-        #if ($line =~ m/From\snoreply@[\w\d|.]+\s+[\w\d|\s|:]+[\d]+$/)
         if (/From\snoreply@[\w\d|.]+\s+[\w\d|\s|:]+[\d]+$/ .. /Status: O/)
         {
         	if ($line =~ m/Subject:\s([\w\d|:|\s|+|?.]+)/)
