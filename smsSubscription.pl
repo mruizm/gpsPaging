a98810a556f382422d7fbe737632165c61181a27
Marco Ruiz <mrmcr82@hotmail.com>
2014-12-11 17:54:42 -0600

    Update smsSubscription.pl

--- smsSubscription.pl
+++ smsSubscription.pl
@@ -26,88 +26,93 @@ print "Messages in SIM\n";
 my $message_number = 0;
 for( $gsm->messages('SM') )
 {
-	my $sender_mobile = $_->sender();
-	my $sender_message = $_->content();
-    print "Sender: $sender_mobile\n";    
+        my $sender_mobile = $_->sender();
+        my $sender_message = $_->content();
+    print "Sender: $sender_mobile\n";
     $sth = $dbh->prepare("SELECT count(*) as count FROM users_info_table
-           			where user_mobile = ?");
-	$sth->execute($sender_mobile) or die $DBI::errstr;
-	$count = $sth->fetchrow_array;
-	print "$count\n";
-	if ($count == 0)
-	{
-		if( $gsm->connect() ) 
-		{		
-			$gsm->register();
-			$gsm->send_sms
-			(
-				recipient => $sender_mobile,
-				content   => $mobile_subs_not_found
-			);		
-		}
-		$gsm->delete_sms($message_number, 'SM');
-		$sth->finish();
-
-	}
-	else
-	{
-		$sth = $dbh->prepare("SELECT user_mobile FROM users_info_table
                                 where user_mobile = ?");
         $sth->execute($sender_mobile) or die $DBI::errstr;
-		while (my @results = $sth->fetchrow()) 
-		{
-			my $db_retrieved_mobile = $results[0];
-			print "Mobile in DB: $db_retrieved_mobile\n";
-			if ($count == 0)
-			{
-				if( $gsm->connect() ) 
-				{		
-					$gsm->register();
-					$gsm->send_sms
-					(
-						recipient => $sender_mobile,
-						content   => $mobile_subs_not_found
-					);		
-				}
-			}
-			else
-			{
-				if ($sender_message eq "SUBSCRIPTION_ON")
-				{
-					$serverDate = strftime("%m/%d/%Y %I:%M %p", localtime());
-					my $update_statement = "UPDATE users_info_table SET user_is_oncall = ?, last_sms_subscribe_on = ? WHERE user_mobile = ?";
-					my $rv = $dbh->do($update_statement, undef, $response_on, $serverDate, $sender_mobile); 
-					$DBI::err && die $DBI::errstr;
-					if( $gsm->connect() ) 
-					{		
-						$gsm->register();
-						$gsm->send_sms
-						(
-							recipient => $sender_mobile,
-							content   => $mobile_subs_on
-						);		
-					}
-				}
-				if ($sender_message eq "SUBSCRIPTION_OFF")
-				{
-					$serverDate = strftime("%m/%d/%Y %I:%M %p", localtime());
-					my $update_statement = "UPDATE users_info_table SET user_is_oncall = ?, last_sms_subscribe_off = ? WHERE user_mobile = ?";
-					my $rv = $dbh->do($update_statement, undef, $response_off, $serverDate, $sender_mobile); 
-					$DBI::err && die $DBI::errstr;
-					if( $gsm->connect() ) 
-					{		
-						$gsm->register();
-						$gsm->send_sms
-						(
-							recipient => $sender_mobile,
-							content   => $mobile_subs_off
-						);		
-					}
-				}
-			}
-			$gsm->delete_sms($message_number, 'SM');			
-		}
-	} 
+        $count = $sth->fetchrow_array;
+        print "$count\n";
+        if ($count == 0)
+        {
+                if( $gsm->connect() )
+                {
+                        $gsm->register();
+                        $gsm->send_sms
+                        (
+                                recipient => $sender_mobile,
+                                content   => $mobile_subs_not_found
+                        );
+                }
+                $gsm->delete_sms($message_number, 'SM');
+                $sth->finish();
+
+        }
+        else
+        {
+                $sth = $dbh->prepare("SELECT user_mobile FROM users_info_table
+                                where user_mobile = ?");
+        $sth->execute($sender_mobile) or die $DBI::errstr;
+                while (my @results = $sth->fetchrow())
+                {
+                        my $db_retrieved_mobile = $results[0];
+                        print "Mobile in DB: $db_retrieved_mobile\n";
+                        if ($count == 0)
+                        {
+                                if( $gsm->connect() )
+                                {
+                                        $gsm->register();
+                                        $gsm->send_sms
+                                        (
+                                                recipient => $sender_mobile,
+                                                content   => $mobile_subs_not_found
+                                        );
+                                }
+                        }
+                        else
+                        {
+                                if ($sender_message eq "SUBSCRIPTION_ON")
+                                {
+                                        $serverDate = strftime("%m/%d/%Y %I:%M %p", localtime());
+                                        my $update_statement = "UPDATE users_info_table SET user_is_oncall = ?, last_sms_subscribe_on = ? WHERE user_mobile = ?";
+                                        my $rv = $dbh->do($update_statement, undef, $response_on, $serverDate, $sender_mobile);
+                                        $DBI::err && die $DBI::errstr;
+                                        if( $gsm->connect() )
+                                        {
+                                                $gsm->register();
+                                                $gsm->send_sms
+                                                (
+                                                        recipient => $sender_mobile,
+                                                        content   => $mobile_subs_on
+                                                );
+                                        }
+                                }
+                                if ($sender_message eq "SUBSCRIPTION_OFF")
+                                {
+                                        $serverDate = strftime("%m/%d/%Y %I:%M %p", localtime());
+                                        my $update_statement = "UPDATE users_info_table SET user_is_oncall = ?, last_sms_subscribe_off = ? WHERE user_mobile = ?";
+                                        my $rv = $dbh->do($update_statement, undef, $response_off, $serverDate, $sender_mobile);
+                                        $DBI::err && die $DBI::errstr;
+                                        if( $gsm->connect() )
+                                        {
+                                                $gsm->register();
+                                                $gsm->send_sms
+                                                (
+                                                        recipient => $sender_mobile,
+                                                        content   => $mobile_subs_off
+                                                );
+                                        }
+                                }
+                        }
+                        $gsm->delete_sms($message_number, 'SM');
+                }
+        }
         #print "Message number:", $message_number, " ", $sender_mobile, ': ', $sender_message, "\n";
         $message_number++;
 }
+#Removal of unnecesarry SMS
+#for (0 .. 9)
+#{
+#       $gsm->delete_sms($_, 'SM');
+#}
